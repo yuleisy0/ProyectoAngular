13ce725ce41deff51927dc84ed03ba1cf6da2834
yuleisy0 <yuleisy.chaves@ucr.ac.cr>
2025-10-16 23:38:00 -0600

    Sidebar ajuste

--- src/app/shared/components/sidebar/sidebar.ts
+++ src/app/shared/components/sidebar/sidebar.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core'; // Importa OnInit si la usas
+import { CommonModule } from '@angular/common'; // 👈 ¡NUEVA IMPORTACIÓN CLAVE!
 import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-sidebar',
-  imports: [ RouterLink, RouterLinkActive],
+  standalone: true, 
+  imports: [
+    CommonModule,       
+    RouterLink,
+    RouterLinkActive
+  ],
   templateUrl: './sidebar.html',
   styleUrl: './sidebar.css'
 })
-export class Sidebar {
+export class Sidebar implements OnInit { 
 
+  isClientesOpen: boolean = false; 
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
 }
